ea4b853a1bff164c41134436804334d448c3fa26
Jorge Vazquez <georgevazj@gmail.com>
2018-12-04 15:18:52 +0100

    Servicios de prueba

---
+++ src/main/java/com/kpmw/restSample/api/AnswerController.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kpmw.restSample.api;
+
+import com.kpmw.restSample.model.Answer;
+import com.kpmw.restSample.repository.AnswerRepository;
+import com.kpmw.restSample.repository.QuestionRepository;
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author jvazquez
+ */
+
+@RestController
+public class AnswerController {
+
+    private AnswerRepository answerRepository;
+    private QuestionRepository questionRepository;
+    
+    @Autowired
+    public AnswerController(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    @Autowired
+    public AnswerController(AnswerRepository answerRepository) {
+        this.answerRepository = answerRepository;
+    }
+    
+    @GetMapping("/questions/{questionId}/answers")
+    public List<Answer> getAnswersByQuestionId(@PathVariable Long questionId) {
+        return answerRepository.findByQuestionId(questionId);
+    }
+    
+    @PostMapping("/questions/{questionId}/answers")
+    public Answer addAnswer(@PathVariable Long questionId,
+                            @Valid @RequestBody Answer answer) {
+        return questionRepository.findById(questionId)
+                .map(question -> {
+                    answer.setQuestion(question);
+                    return answerRepository.save(answer);
+                }).orElseThrow(() -> new ResourceNotFoundException("Question not found with id " + questionId));
+    }
+    
+    @PutMapping("/questions/{questionId}/answers/{answerId}")
+    public Answer updateAnswer(@PathVariable Long questionId,
+                               @PathVariable Long answerId,
+                               @Valid @RequestBody Answer answerRequest) {
+        if(!questionRepository.existsById(questionId)) {
+            throw new ResourceNotFoundException("Question not found with id " + questionId);
+        }
+        return answerRepository.findById(answerId)
+                .map(answer -> {
+                    answer.setText(answerRequest.getText());
+                    return answerRepository.save(answer);
+                }).orElseThrow(() -> new ResourceNotFoundException("Answer not found with id " + answerId));
+    }
+    
+}
